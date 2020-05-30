27e34570d0d42912ae297d1109bc12b49a39d4b7
cesar <cesar@panoimagen.com>
2020-05-30 18:09:45 +0200

    Antes de instalar el hotspot

--- bichos.py
+++ bichos.py
@@ -3,37 +3,41 @@ import commands
 
 bicho = sys.argv[1]
 orden = sys.argv[2]
-respuesta = ''
-print (bicho)
-print (orden)
 
 if bicho == '1' or bicho == '9':
 	if orden == '0':
 		commands.getoutput('mosquitto_pub -h 192.168.1.191 -t casa/1 -m "0"')
-		respuesta += 'Bicho 1 Apagado'
+		respuesta = 'Bicho 1 Apagado'
 	elif orden == '1':
 		commands.getoutput('mosquitto_pub -h 192.168.1.191 -t casa/1 -m "1"')
-		respuesta += 'Bicho 1 Encendido'
+		respuesta = 'Bicho 1 Encendido'
+	elif orden == '2':
+		commands.getoutput('mosquitto_pub -h 192.168.1.191 -t casa/1 -m "2"')
+		aux = commands.getoutput('mosquitto_sub -h 192.168.1.191 -t casa/1 -C 1 -W 5')
+		respuesta = ''.join(['Bicho 1 ', aux])
 	elif orden == '9':
 		commands.getoutput('mosquitto_pub -h 192.168.1.191 -t casa/1 -m "9"')
-		aux = commands.getoutput('mosquitto_sub -h 192.168.1.191 -t casa/1 -C 1')
-		respuesta += 'Bicho 1 '
-		respuesta += aux
+		aux = commands.getoutput('mosquitto_sub -h 192.168.1.191 -t casa/1 -C 1 -W 5')
+		respuesta = ''.join(['Bicho 1 ', aux])
 	else:
-		respuesta += 'Error en argumentos'
+		respuesta = 'Error en argumentos'
+	print (respuesta)
+
 if bicho == '2' or bicho == '9':
 	if orden == '0':
 		commands.getoutput('mosquitto_pub -h 192.168.1.191 -t casa/2 -m "0"')
-		respuesta += 'Bicho 2 Apagado'
+		respuesta = 'Bicho 2 Apagado'
 	elif orden == '1':
 		commands.getoutput('mosquitto_pub -h 192.168.1.191 -t casa/2 -m "1"')
-		respuesta += 'Bicho 2 Encendido'
+		respuesta = 'Bicho 2 Encendido'
+	elif orden == '2':
+		commands.getoutput('mosquitto_pub -h 192.168.1.191 -t casa/2 -m "2"')
+		aux = commands.getoutput('mosquitto_sub -h 192.168.1.191 -t casa/2 -C 1 -W 5')
+		respuesta = ''.join(['Bicho 2 ', aux])
 	elif orden == '9':
 		commands.getoutput('mosquitto_pub -h 192.168.1.191 -t casa/2 -m "9"')
-		aux = commands.getoutput('mosquitto_sub -h 192.168.1.191 -t casa/2 -C 1')
-		respuesta += 'Bicho 2 '
-		respuesta += aux
+		aux = commands.getoutput('mosquitto_sub -h 192.168.1.191 -t casa/2 -C 1 -W 5')
+		respuesta = ''.join(['Bicho 2 ', aux])
 	else:
-		respuesta += 'Error en argumentos'
-
-print (respuesta)
+		respuesta = 'Error en argumentos'
+	print (respuesta)
--- opcion.py
+++ opcion.py
@@ -22,164 +22,173 @@ DIRECTORIO = '/home/pi/proyecto/'
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-	lista = message.text.split()
-        orden = lista[0].upper()
+    lista = message.text.split()
+    orden = lista[0].upper()
 
-        # lista = orden = ''
-        if orden == 'PRINCIPAL' or orden == '.':
-                panelPrincipal = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=False)
-                panelPrincipal.add('Alarmas', 'Compras', 'Domotica', 'Sistema')
-                bot.send_message(message.chat.id, "Menu principal ...", reply_markup=panelPrincipal)
+    if orden == 'PRINCIPAL' or orden == '.':
+        panelPrincipal = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=False)
+        panelPrincipal.add('Alarmas', 'Compras', 'Domotica', 'Sistema')
+        bot.send_message(message.chat.id, "Menu principal ...", reply_markup=panelPrincipal)
 
-	if orden == 'ALARMAS':
-		panelAlarmas = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=False)
-		panelAlarmas.add('Borrar', 'Consultar', 'Crear', 'Principal')
-	        bot.send_message(message.chat.id, "Menu alarmas ...", reply_markup=panelAlarmas)
+    if orden == 'ALARMAS':
+        panelAlarmas = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=False)
+        panelAlarmas.add('Borrar', 'Consultar', 'Crear', 'Principal')
+        bot.send_message(message.chat.id, "Menu alarmas ...", reply_markup=panelAlarmas)
 
-        if orden == 'COMPRAS':
-                panelCompras = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True, one_time_keyboard=False)
-                panelCompras.add('Limpiar', 'Cesta', 'Apuntar', 'Desapuntar', 'Principal')
-                bot.send_message(message.chat.id, "Menu compras ...", reply_markup=panelCompras)
+    if orden == 'COMPRAS':
+        panelCompras = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True, one_time_keyboard=False)
+        panelCompras.add('Limpiar', 'Cesta', 'Apuntar', 'Desapuntar', 'Principal')
+        bot.send_message(message.chat.id, "Menu compras ...", reply_markup=panelCompras)
 
-        if orden == 'DOMOTICA':
-                panelDomotica = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True, one_time_keyboard=False)
-                panelDomotica.add('Bichos', 'Foto', 'Principal')
-                bot.send_message(message.chat.id, "Menu domotica ...", reply_markup=panelDomotica)
+    if orden == 'DOMOTICA':
+        panelDomotica = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True, one_time_keyboard=False)
+        panelDomotica.add('Bichos', 'Foto', 'Principal')
+        bot.send_message(message.chat.id, "Menu domotica ...", reply_markup=panelDomotica)
 
-	if orden == 'SISTEMA':
-                panelSistema = types.ReplyKeyboardMarkup(row_width=4, resize_keyboard=True, one_time_keyboard=False)
-                panelSistema.add('Reiniciar', 'Apagar', 'Info', 'Principal')
-                bot.send_message(message.chat.id, "Menu sistema ...", reply_markup=panelSistema)
+    if orden == 'SISTEMA':
+        panelSistema = types.ReplyKeyboardMarkup(row_width=4, resize_keyboard=True, one_time_keyboard=False)
+        panelSistema.add('Reiniciar', 'Apagar', 'Info', 'Principal')
+        bot.send_message(message.chat.id, "Menu sistema ...", reply_markup=panelSistema)
 
-	############################### ALARMAS ###############################
+    ############################### ALARMAS ###############################
 
-	if orden == 'BORRAR':
-                borrado = False
-                if len(lista) > 1:
-                        cadena = ''.join(['sudo python ', DIRECTORIO, 'borrar.py ' , lista[1]])
-                        aux = commands.getoutput(cadena)
-                        bot.send_message(message.chat.id, "Alarma borrada")
-                        borrado = True
-                if len(lista) == 1 or borrado:
-                        cadena = ''.join(['sudo python  ', DIRECTORIO, 'consulta.py corto'])
-                        listaAlarmas = commands.getoutput(cadena).split('\n')
-                        panelBorrar = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=False)
-                        acumulados = 0
-                        for articulo in listaAlarmas:
-                                if acumulados == 0:
-                                        acumulados = 1
-                                        aux = 'Borrar ' + articulo
-                                elif acumulados == 1:
-                                        acumulados = 0
-                                        aux1 = 'Borrar ' + articulo
-                                        panelBorrar.add(aux, aux1)
-                        if acumulados == 1:
-                                panelBorrar.add(aux)
-                        panelBorrar.add('Alarmas', 'Principal')
-                        bot.send_message(message.chat.id, "Pulsa para borrar ...", reply_markup=panelBorrar)
+    if orden == 'BORRAR':
+        borrado = False
+        if len(lista) > 1:
+            cadena = ''.join(['sudo python ', DIRECTORIO, 'borrar.py ' , lista[1]])
+            aux = commands.getoutput(cadena)
+            bot.send_message(message.chat.id, "Alarma borrada")
+            borrado = True
+        if len(lista) == 1 or borrado:
+            cadena = ''.join(['sudo python  ', DIRECTORIO, 'consulta.py corto'])
+            listaAlarmas = commands.getoutput(cadena).split('\n')
+            panelBorrar = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=False)
+            acumulados = 0
+            for articulo in listaAlarmas:
+                if acumulados == 0:
+                    acumulados = 1
+                    aux = 'Borrar ' + articulo
+                elif acumulados == 1:
+                    acumulados = 0
+                    aux1 = 'Borrar ' + articulo
+                    panelBorrar.add(aux, aux1)
+            if acumulados == 1:
+                panelBorrar.add(aux)
+            panelBorrar.add('Alarmas', 'Principal')
+            bot.send_message(message.chat.id, "Pulsa para borrar ...", reply_markup=panelBorrar)
 
-        if orden == 'CONSULTAR':
-	        cadena = ''.join(['sudo python  ', DIRECTORIO, 'consulta.py largo'])
-        	aux = commands.getoutput(cadena)
-	        bot.send_message(message.chat.id, aux)
+    if orden == 'CONSULTAR':
+        cadena = ''.join(['sudo python  ', DIRECTORIO, 'consulta.py largo'])
+        aux = commands.getoutput(cadena)
+        bot.send_message(message.chat.id, aux)
 
-        if orden == 'CREAR':
-		if len(lista) != 7:
-	        	bot.send_message(message.chat.id, "Para poner otra alarma, escribe ...\n      crear minuto hora dia mes dia_semana mensaje\n usando x para comodin")
-		elif not(lista[1]=='x' or (int(lista[1])>=0 and int(lista[1])<=59)):
-	               	bot.send_message(message.chat.id, 'Error en la definicion de los minutos')
-	        elif not(lista[2]=='x' or (int(lista[2])>=0 and int(lista[2])<=23)):
-        	       	bot.send_message(message.chat.id, 'Error en la definicion de las horas')
-	        elif not(lista[3]=='x' or (int(lista[3])>=1 and int(lista[3])<=31)):
-		        bot.send_message(message.chat.id, 'Error en la definicion de el dia del mes')
-		elif not(lista[4]=='x' or (int(lista[4])>=1 and int(lista[4])<=12)):
-        	        bot.send_message(message.chat.id, 'Error en la definicion de el mes')
-		elif not(lista[5]=='x' or (int(lista[5])>=1 and int(lista[5])<=7)):
-        	        bot.send_message(message.chat.id, 'Error en la definicion de el dia de la semana')
-		elif lista[6]=='':
-        	        bot.send_message(message.chat.id, 'Error en la definicion del texto del mensaje')
-		else:
-			cadena = ''.join(['sudo python  ', DIRECTORIO, 'alarma.py ' , lista[1] , ' ' ,lista[2] , ' ' , lista[3] , ' ' , lista[4] , ' ', lista[5] , ' ' , lista[6]])
-		        aux = commands.getoutput(cadena)
-        		bot.send_message(message.chat.id, 'Alarma correctamente definida y guardada')
+    if orden == 'CREAR':
+        if len(lista) != 7:
+            bot.send_message(message.chat.id, "Para poner otra alarma, escribe ...\n      crear minuto hora dia mes dia_semana mensaje\n usando x para comodin")
+        elif not(lista[1]=='x' or (int(lista[1])>=0 and int(lista[1])<=59)):
+            bot.send_message(message.chat.id, 'Error en la definicion de los minutos')
+        elif not(lista[2]=='x' or (int(lista[2])>=0 and int(lista[2])<=23)):
+            bot.send_message(message.chat.id, 'Error en la definicion de las horas')
+        elif not(lista[3]=='x' or (int(lista[3])>=1 and int(lista[3])<=31)):
+            bot.send_message(message.chat.id, 'Error en la definicion de el dia del mes')
+        elif not(lista[4]=='x' or (int(lista[4])>=1 and int(lista[4])<=12)):
+            bot.send_message(message.chat.id, 'Error en la definicion de el mes')
+        elif not(lista[5]=='x' or (int(lista[5])>=1 and int(lista[5])<=7)):
+            bot.send_message(message.chat.id, 'Error en la definicion de el dia de la semana')
+        elif lista[6]=='':
+            bot.send_message(message.chat.id, 'Error en la definicion del texto del mensaje')
+        else:
+            cadena = ''.join(['sudo python  ', DIRECTORIO, 'alarma.py ' , lista[1] , ' ' ,lista[2] , ' ' , lista[3] , ' ' , lista[4] , ' ', lista[5] , ' ' , lista[6]])
+            aux = commands.getoutput(cadena)
+            bot.send_message(message.chat.id, 'Alarma correctamente definida y guardada')
 
-	############################### COMPRAS ###############################
+    ############################### COMPRAS ###############################
 
-        if orden == 'LIMPIAR':
-                cadena = ''.join(['sudo python  ', DIRECTORIO, 'limpiar.py'])
-                aux = commands.getoutput(cadena)
-                aux = ''.join([aux , '\n... la cesta esta vacia ahora'])
-                bot.send_message(message.chat.id, aux)
+    if orden == 'LIMPIAR':
+        cadena = ''.join(['sudo python  ', DIRECTORIO, 'limpiar.py'])
+        aux = commands.getoutput(cadena)
+        aux = ''.join([aux , '\n... la cesta esta vacia ahora'])
+        bot.send_message(message.chat.id, aux)
 
-        if orden == 'CESTA':
-	        cadena = ''.join(['sudo python ', DIRECTORIO, 'cesta.py'])
-        	aux = commands.getoutput(cadena)
-		if aux == '':
-		        aux = ''.join([aux , 'La cesta esta vacia'])
-		else:
-		        aux = ''.join([aux , '\n... esta pendiente de comprar'])
-        	bot.send_message(message.chat.id, aux)
+    if orden == 'CESTA':
+        cadena = ''.join(['sudo python ', DIRECTORIO, 'cesta.py'])
+        aux = commands.getoutput(cadena)
+        if aux == '':
+            aux = ''.join([aux , 'La cesta esta vacia'])
+        else:
+            aux = ''.join([aux , '\n... esta pendiente de comprar'])
+        bot.send_message(message.chat.id, aux)
 
-        if orden == 'DESAPUNTAR':                    # esta opcion se deriva a la opcion 'comprar'
-		desapuntado = False
-		if len(lista) > 1:
-                        cadena = ''.join(['sudo python  ', DIRECTORIO, 'desapuntar.py ' , lista[1]])
-			aux = commands.getoutput(cadena)
-                        bot.send_message(message.chat.id, "Producto desapuntado")
-			desapuntado = True
-		if len(lista) == 1 or desapuntado:
- 	                cadena = ''.join(['sudo python  ', DIRECTORIO, 'cesta.py'])
-        		listaCompra = commands.getoutput(cadena).split()
-		      	panelDesapuntar = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=False)
-			acumulados = 0
-			for articulo in listaCompra:
-				if acumulados == 0:
-					acumulados = 1
-					aux = 'Desapuntar ' + articulo
-				elif acumulados == 1:
-					acumulados = 0
-					aux1 = 'Desapuntar ' + articulo
-					panelDesapuntar.add(aux, aux1)
-			if acumulados == 1:
-				panelDesapuntar.add(aux)
-       	        	panelDesapuntar.add('Compras', 'Principal')
-        	        bot.send_message(message.chat.id, "Pulsa para desapuntar ...", reply_markup=panelDesapuntar)
+    if orden == 'DESAPUNTAR':                    # esta opcion se deriva a la opcion 'comprar'
+        desapuntado = False
+        if len(lista) > 1:
+            cadena = ''.join(['sudo python  ', DIRECTORIO, 'desapuntar.py ' , lista[1]])
+            aux = commands.getoutput(cadena)
+            bot.send_message(message.chat.id, "Producto desapuntado")
+            desapuntado = True
+        if len(lista) == 1 or desapuntado:
+            cadena = ''.join(['sudo python  ', DIRECTORIO, 'cesta.py'])
+            listaCompra = commands.getoutput(cadena).split()
+            panelDesapuntar = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=False)
+            acumulados = 0
+            for articulo in listaCompra:
+                if acumulados == 0:
+                    acumulados = 1
+                    aux = 'Desapuntar ' + articulo
+                elif acumulados == 1:
+                    acumulados = 0
+                    aux1 = 'Desapuntar ' + articulo
+                    panelDesapuntar.add(aux, aux1)
+            if acumulados == 1:
+                panelDesapuntar.add(aux)
+            panelDesapuntar.add('Compras', 'Principal')
+            bot.send_message(message.chat.id, "Pulsa para desapuntar ...", reply_markup=panelDesapuntar)
 
-        if orden == 'APUNTAR':
-		if len(lista) != 2:
-	        	bot.send_message(message.chat.id, "Para comprar, escribe ...\n      apuntar mi_producto")
-		else:
-	        	cadena = ''.join(['sudo python  ', DIRECTORIO, 'compra.py ' , unidecode(lista[1])])
-	        	aux = commands.getoutput(cadena)
-		        bot.send_message(message.chat.id, "Producto apuntado")
+    if orden == 'APUNTAR':
+        if len(lista) == 1:
+            bot.send_message(message.chat.id, "Para comprar, escribe ...\n      apuntar mi_producto")
+        else:
+            cadena = ''.join(['sudo python  ', DIRECTORIO, 'compra.py ' , unidecode(lista[1])])
+            aux = commands.getoutput(cadena)
+            bot.send_message(message.chat.id, "Producto apuntado")
 
-	############################### DOMOTICA ###############################
+    ############################### DOMOTICA ###############################
 
-        if orden == 'BICHOS':
-      		cadena = ''.join(['sudo python  ', DIRECTORIO, 'bichos.py 1 9'])
-		aux = commands.getoutput(cadena)
-		print (aux)
-      		cadena = ''.join(['sudo python  ', DIRECTORIO, 'bichos.py 2 9'])
-                aux = commands.getoutput(cadena)
-                print (aux)
+    if orden == 'BICHOS':
+        refrescar = False
+        if len(lista) == 3:
+            cadena = ''.join(['sudo python  ', DIRECTORIO, 'bichos.py ', lista[1], ' ', lista[2]])
+            commands.getoutput(cadena).split()
+            refrescar = True
+        if len(lista) == 1 or refrescar:
+            cadena = ''.join(['sudo python  ', DIRECTORIO, 'bichos.py 9 9'])
+            listaBichos = commands.getoutput(cadena).split()
+            panelBichos = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True, one_time_keyboard=False)
+            for linea in range(0, len(listaBichos), 3):
+                idBicho = listaBichos[linea] + 's ' + listaBichos[linea + 1] + ' '
+                panelBichos.add( idBicho + listaBichos[linea + 2], idBicho + '0', idBicho + '1')
+            panelBichos.add('Bichos', 'Principal')
+            bot.send_message(message.chat.id, "Pulsa para encender o apagar ...", reply_markup=panelBichos)
+        else:
+            bot.send_message(message.chat.id, "Numero de argumentos chungaletis")
 
-	if orden == 'FOTO':
-		aux = commands.getoutput(''.join(['sudo python ', DIRECTORIO, 'foto.py']))
-		bot.send_photo(message.chat.id, photo=open(aux, 'rb'))
+    if orden == 'FOTO':
+        aux = commands.getoutput(''.join(['sudo python ', DIRECTORIO, 'foto.py']))
+        bot.send_photo(message.chat.id, photo=open(aux, 'rb'))
 
-	############################### SISTEMA ###############################
+    ############################### SISTEMA ###############################
 
-        if orden == 'REINICIAR':
-                aux = commands.getoutput('sudo reboot')
-                bot.send_message(message.chat.id, aux)
+    if orden == 'REINICIAR':
+        aux = commands.getoutput('sudo reboot')
+        bot.send_message(message.chat.id, aux)
 
-        if orden == 'APAGAR':
-		aux = commands.getoutput('sudo shutdown 0')
-		bot.send_message(message.chat.id, aux)
+    if orden == 'APAGAR':
+        aux = commands.getoutput('sudo shutdown 0')
+        bot.send_message(message.chat.id, aux)
 
-        if orden == 'INFO':
-                aux = commands.getoutput(''.join(['sudo ', DIRECTORIO, 'info.sh']))
-                bot.send_message(message.chat.id, aux)
+    if orden == 'INFO':
+        aux = commands.getoutput(''.join(['sudo ', DIRECTORIO, 'info.sh']))
+        bot.send_message(message.chat.id, aux)
 
 time.sleep(10)
 print ("EJECUTANDO")
